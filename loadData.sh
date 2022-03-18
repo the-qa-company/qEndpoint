e1a76663c58d6b17d967f53083ff96086e2b32cb
J.M <jonathan.mallet3872@gmail.com>
2022-03-18 11:42:42 +0100

    add option for wget so that it doesnt lost connection

--- loadData.sh
+++ loadData.sh
@@ -3,8 +3,9 @@ if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
     echo "starting..."
-    echo "Downloading the HDT index..." && wget --progress=bar:force:noscroll -O /home/app/data/hdt-store/index.hdt https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt \
-        && wget --progress=bar:force:noscroll -O /home/app/data/hdt-store/index.hdt.index.v1-1 https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt.index.v1-1
+    echo "Downloading the HDT index..."
+    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O /home/app/data/hdt-store/index.hdt https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt
+    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O /home/app/data/hdt-store/index.hdt.index.v1-1 https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt.index.v1-1
 fi
 echo "Starting HDT Sparql Service..."
 java -Xmx"$1" -Dspring.config.location=file:///home/app/application-prod.properties -jar /usr/local/lib/hdtSparqlEndpoint-*-SNAPSHOT.jar
