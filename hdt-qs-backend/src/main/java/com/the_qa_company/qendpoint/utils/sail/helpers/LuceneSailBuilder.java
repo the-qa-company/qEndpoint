19ba6999b804fbd7033c6d05397779629ed02645
qaate47 <antoine.willerval@the-qa-company.com>
2022-05-11 15:53:36 +0200

    add lucene rdf builder

--- hdt-qs-backend/src/main/java/com/the_qa_company/qendpoint/utils/sail/helpers/LuceneSailBuilder.java
+++ hdt-qs-backend/src/main/java/com/the_qa_company/qendpoint/utils/sail/helpers/LuceneSailBuilder.java
@@ -1,24 +1,37 @@
 package com.the_qa_company.qendpoint.utils.sail.helpers;
 
+import com.the_qa_company.qendpoint.compiler.SailCompilerSchema;
 import com.the_qa_company.qendpoint.utils.sail.linked.LinkedSail;
 import com.the_qa_company.qendpoint.utils.sail.linked.SimpleLinkedSail;
+import org.eclipse.rdf4j.model.BNode;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.sail.evaluation.TupleFunctionEvaluationMode;
 import org.eclipse.rdf4j.sail.lucene.LuceneSail;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * helper class to create a {@link org.eclipse.rdf4j.sail.lucene.LuceneSail} linked or not
  * @author Antoine Willerval
  */
 public class LuceneSailBuilder {
-	private LuceneSail sail;
+	private final Map<String, String> params = new HashMap<>();
+	private TupleFunctionEvaluationMode eval;
+	private String reindexQuery;
 
 	/**
 	 * create a builder with default config
 	 */
 	public LuceneSailBuilder() {
 		// default config
-		sail = new LuceneSail();
-		sail.setParameter(LuceneSail.INDEX_CLASS_KEY, LuceneSail.DEFAULT_INDEX_CLASS);
+		withParameter(LuceneSail.INDEX_CLASS_KEY, LuceneSail.DEFAULT_INDEX_CLASS);
 	}
 
 	/**
@@ -28,7 +41,7 @@ public class LuceneSailBuilder {
 	 * @return this
 	 */
 	public LuceneSailBuilder withParameter(String key, String value) {
-		sail.setParameter(key, value);
+		params.put(key, value);
 		return this;
 	}
 
@@ -65,7 +78,7 @@ public class LuceneSailBuilder {
 	 * @return this
 	 */
 	public LuceneSailBuilder withEvaluationMode(TupleFunctionEvaluationMode mode) {
-		sail.setEvaluationMode(mode);
+		eval = mode;
 		return this;
 	}
 
@@ -75,7 +88,7 @@ public class LuceneSailBuilder {
 	 * @return this
 	 */
 	public LuceneSailBuilder withReindexQuery(String query) {
-		sail.setReindexQuery(query);
+		reindexQuery = query;
 		return this;
 	}
 
@@ -84,12 +97,43 @@ public class LuceneSailBuilder {
 	 * @return the sail
 	 */
 	public LuceneSail build() {
-		if (sail == null) {
-			throw new IllegalArgumentException("build() method was already called before!");
+		LuceneSail sail = new LuceneSail();
+		params.forEach(sail::setParameter);
+		if (eval != null) {
+			sail.setEvaluationMode(eval);
 		}
-		LuceneSail tmp = sail;
-		sail = null;
-		return tmp;
+		if (reindexQuery != null) {
+			sail.setReindexQuery(reindexQuery);
+		}
+		return sail;
+	}
+
+	/**
+	 * build this lucene sail to rdf for compiling with the {@link com.the_qa_company.qendpoint.compiler.SailCompiler}
+	 * @param node node describing this sail
+	 * @return iterator of statements
+	 */
+	public Iterator<Statement> buildToRDF(Resource node) {
+		ValueFactory vf = SimpleValueFactory.getInstance();
+		List<Statement> list = new ArrayList<>();
+		// mdlc:type mdlc:luceneNode
+		list.add(vf.createStatement(node, SailCompilerSchema.TYPE, SailCompilerSchema.LUCENE_TYPE));
+		params.forEach((key, value) -> {
+			// mdlc:luceneParam [...]
+			BNode param = vf.createBNode();
+			list.add(vf.createStatement(node, SailCompilerSchema.LUCENE_TYPE_PARAM, param));
+			list.add(vf.createStatement(param, SailCompilerSchema.PARAM_KEY, vf.createLiteral(key)));
+			list.add(vf.createStatement(param, SailCompilerSchema.PARAM_VALUE, vf.createLiteral(key)));
+		});
+		// mdlc:luceneEvalMode "MODE"
+		if (eval != null) {
+			list.add(vf.createStatement(node, SailCompilerSchema.LUCENE_TYPE_EVAL_MODE, vf.createLiteral(eval.name())));
+		}
+		// mdlc:luceneReindexQuery "SELECT ..."
+		if (reindexQuery != null) {
+			list.add(vf.createStatement(node, SailCompilerSchema.LUCENE_TYPE_REINDEX_QUERY, vf.createLiteral(reindexQuery)));
+		}
+		return list.iterator();
 	}
 
 	/**
