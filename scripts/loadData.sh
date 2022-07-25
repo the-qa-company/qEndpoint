7d90eec21b7120eea75c9936e21f354f5d4166d8
J.M <jonathan.mallet3872@gmail.com>
2022-07-25 10:49:24 +0200

    Fix download not restarting on stop

--- scripts/loadData.sh
+++ scripts/loadData.sh
@@ -8,14 +8,14 @@ else
     echo "Downloading the HDT index..."
     mkdir -p qendpoint/hdt-store
 
-    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt -o index_big.hdt.tmp
-    mv index_big.hdt.tmp index_big.hdt
+    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE.tmp https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt 
+    mv $FILE.tmp $FILE
 
 fi
 
 if [ -f "$FILE2" ]; then
     echo "$FILE2 exists."
 else
-  wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE.index.v1-1 https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt.index.v1-1 -o index_big.hdt.index.v1-1.tmp
-  mv index_big.hdt.index.v1-1.tmp index_big.hdt.index.v1-1
+  wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE2.tmp https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt.index.v1-1 
+  mv $FILE2.tmp $FILE2
 fi
