f3873a57eec0470dabc41a19f257d25968b2a9d9
qaate47 <antoine.willerval@the-qa-company.com>
2022-08-31 14:26:35 +0200

    GH-108 Fix FormatUtils' header methods

--- hdt-qs-backend/src/main/java/com/the_qa_company/qendpoint/utils/FormatUtils.java
+++ hdt-qs-backend/src/main/java/com/the_qa_company/qendpoint/utils/FormatUtils.java
@@ -5,7 +5,11 @@ import org.eclipse.rdf4j.query.resultio.TupleQueryResultWriterRegistry;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Utility class to get response format
@@ -13,6 +17,34 @@ import java.util.Optional;
  * @author Antoine Willerval
  */
 public class FormatUtils {
+	private static class ValuedMime {
+		float value;
+		String mime;
+
+		public ValuedMime(float value, String mime) {
+			this.value = value;
+			this.mime = mime;
+		}
+	}
+
+	private static Stream<ValuedMime> sortedFormat(String acceptHeader) {
+
+		List<ValuedMime> mimes = new ArrayList<>();
+
+		for (String acceptedType : acceptHeader.split("[,]")) {
+			String[] split = acceptedType.split(";");
+
+			String mime = split[0];
+			float value;
+			if (split.length != 1 && split[1].startsWith("q=")) {
+				value = Float.parseFloat(split[1].substring(2));
+			} else {
+				value = 1;
+			}
+			mimes.add(new ValuedMime(value, mime));
+		}
+		return mimes.stream().sorted(Comparator.<ValuedMime>comparingDouble(m -> m.value).reversed());
+	}
 
 	/**
 	 * get a result writer format for an accept header
@@ -21,14 +53,9 @@ public class FormatUtils {
 	 * @return the format
 	 */
 	public static Optional<QueryResultFormat> getResultWriterFormat(String acceptHeader) {
-		for (String mime : acceptHeader.split("[,;]")) {
-			Optional<QueryResultFormat> format = TupleQueryResultWriterRegistry.getInstance()
-					.getFileFormatForMIMEType(mime);
-			if (format.isPresent()) {
-				return format;
-			}
-		}
-		return Optional.empty();
+		return sortedFormat(acceptHeader)
+				.map(m -> TupleQueryResultWriterRegistry.getInstance().getFileFormatForMIMEType(m.mime))
+				.filter(Optional::isPresent).map(Optional::get).findFirst();
 	}
 
 	/**
@@ -38,12 +65,7 @@ public class FormatUtils {
 	 * @return the format
 	 */
 	public static Optional<RDFFormat> getRDFWriterFormat(String acceptHeader) {
-		for (String mime : acceptHeader.split("[,;]")) {
-			Optional<RDFFormat> format = Rio.getWriterFormatForMIMEType(mime);
-			if (format.isPresent()) {
-				return format;
-			}
-		}
-		return Optional.empty();
+		return sortedFormat(acceptHeader).map(m -> Rio.getWriterFormatForMIMEType(m.mime)).filter(Optional::isPresent)
+				.map(Optional::get).findFirst();
 	}
 }
