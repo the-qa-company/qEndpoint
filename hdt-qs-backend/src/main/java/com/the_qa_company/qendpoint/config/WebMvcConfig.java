b6f31165cc5778e59439ed474703c6c2bbfe893c
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2022-10-22 15:57:11 +0200

    Proper fomratting according to repo rules

--- hdt-qs-backend/src/main/java/com/the_qa_company/qendpoint/config/WebMvcConfig.java
+++ hdt-qs-backend/src/main/java/com/the_qa_company/qendpoint/config/WebMvcConfig.java
@@ -8,15 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-  // this is the configuration necessary for the react front-end, it redirects to index.html if no match
-  @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/").setViewName("forward:/index.html");
-    // Single directory level - no need to exclude "api"
-    registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/index.html");
-    // Multi-level directory path, need to exclude "api" on the first part of the path
+	// this is the configuration necessary for the react front-end, it redirects
+	// to index.html if no match
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.html");
+		// Single directory level - no need to exclude "api"
+		registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/index.html");
+		// Multi-level directory path, need to exclude "api" on the first part
+		// of the path
 //    registry
 //        .addViewController("/{x:^(?!api$).*$}/{y:[\\w\\-]+}")
 //        .setViewName("forward:/index.html");
-  }
+	}
 }
