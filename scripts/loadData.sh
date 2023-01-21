c2155f85c95c585f8cb5f7917b01b18bdb74c7e9
qaate47 <antoine.willerval@the-qa-company.com>
2023-01-21 08:56:45 +0100

    GH-225 add Wikidata-all docker image

--- scripts/loadData.sh
+++ scripts/loadData.sh
@@ -1,14 +1,16 @@
 FILE=/app/qendpoint/hdt-store/index_dev.hdt
 FILE2=$FILE.index.v1-1
+CDN=https://qanswer-svc4.univ-st-etienne.fr
+HDT_BASE=index_big
 
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else
     echo "starting..."
-    echo "Downloading the HDT index..."
+    echo "Downloading the HDT index $HDT_BASE..."
     mkdir -p qendpoint/hdt-store
 
-    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE.tmp https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt 
+    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE.tmp "$CDN/$HDT_BASE.hdt"
     mv $FILE.tmp $FILE
 
 fi
@@ -16,6 +18,7 @@ fi
 if [ -f "$FILE2" ]; then
     echo "$FILE2 exists."
 else
-  wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE2.tmp https://qanswer-svc4.univ-st-etienne.fr/index_big.hdt.index.v1-1 
-  mv $FILE2.tmp $FILE2
+    echo "Downloading the HDT co-index $HDT_BASE..."
+    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE2.tmp "$CDN/$HDT_BASE.hdt.index.v1-1"
+    mv $FILE2.tmp $FILE2
 fi
---
+++ scripts/loadDataAll.sh
@@ -0,0 +1,24 @@
+FILE=/app/qendpoint/hdt-store/index_dev.hdt
+FILE2=$FILE.index.v1-1
+CDN=https://qanswer-svc4.univ-st-etienne.fr
+HDT_BASE=wikidata_all
+
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    echo "starting..."
+    echo "Downloading the HDT index $HDT_BASE..."
+    mkdir -p qendpoint/hdt-store
+
+    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE.tmp "$CDN/$HDT_BASE.hdt"
+    mv $FILE.tmp $FILE
+
+fi
+
+if [ -f "$FILE2" ]; then
+    echo "$FILE2 exists."
+else
+    echo "Downloading the HDT co-index $HDT_BASE..."
+    wget --progress=bar:force:noscroll -c --retry-connrefused --tries 0 --timeout 10 -O $FILE2.tmp "$CDN/$HDT_BASE.hdt.index.v1-1"
+    mv $FILE2.tmp $FILE2
+fi
