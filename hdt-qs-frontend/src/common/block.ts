8f5c584a82b4ce35bd0a7525de389e7174b28c51
Clement Defretiere <defretiere.c@gmail.com>
2023-03-20 12:21:39 +0100

    GH-237 Replace prompt hook

--- hdt-qs-frontend/src/common/block.ts
+++ hdt-qs-frontend/src/common/block.ts
@@ -1,63 +1,50 @@
-import type { Blocker, History, Transition } from 'history'
-import { ContextType, useCallback, useContext, useEffect } from 'react'
-import { Navigator as BaseNavigator, UNSAFE_NavigationContext as NavigationContext } from 'react-router-dom'
-
-interface Navigator extends BaseNavigator {
-    block: History['block'];
-}
-
-type NavigationContextWithBlock = ContextType<typeof NavigationContext> & { navigator: Navigator };
-
 /**
- * @source https://github.com/remix-run/react-router/commit/256cad70d3fd4500b1abcfea66f3ee622fb90874
+ * Prompts a user when they exit the page
+ * https://gist.github.com/MarksCode/64e438c82b0b2a1161e01c88ca0d0355
  */
-export function useBlocker (blocker: Blocker, when = true) {
-  const { navigator } = useContext(NavigationContext) as NavigationContextWithBlock
+
+import { useCallback, useContext, useEffect } from 'react'
+import { UNSAFE_NavigationContext as NavigationContext } from 'react-router-dom'
+
+function useConfirmExit (confirmExit: () => boolean, when = true) {
+  const { navigator } = useContext(NavigationContext)
 
   useEffect(() => {
     if (!when) {
       return
     }
 
-    const unblock = navigator.block((tx: Transition) => {
-      const autoUnblockingTx = {
-        ...tx,
-        retry () {
-          // Automatically unblock the transition so it can play all the way
-          // through before retrying it. TODO: Figure out how to re-enable
-          // this block if the transition is cancelled for some reason.
-          unblock()
-          tx.retry()
-        }
+    const push = navigator.push
+
+    navigator.push = (...args: Parameters<typeof push>) => {
+      const result = confirmExit()
+      if (result !== false) {
+        push(...args)
       }
+    }
 
-      blocker(autoUnblockingTx)
-    })
-
-    return unblock
-  }, [navigator, blocker, when])
+    return () => {
+      navigator.push = push
+    }
+  }, [navigator, confirmExit, when])
 }
 
-/**
- * @source https://github.com/remix-run/react-router/issues/8139#issuecomment-1021457943
- */
-export function usePrompt (message: string | ((
-    location: Transition['location'],
-    action: Transition['action'],
-) => string), when = true) {
-  const blocker = useCallback((tx: Transition) => {
-    let response
-    if (typeof message === 'function') {
-      response = message(tx.location, tx.action)
-      if (typeof response === 'string') {
-        response = window.confirm(response)
+export function usePrompt (message: string, when = true) {
+  useEffect(() => {
+    if (when) {
+      window.onbeforeunload = function () {
+        return message
       }
-    } else {
-      response = window.confirm(message)
     }
-    if (response) {
-      tx.retry()
+
+    return () => {
+      window.onbeforeunload = null
     }
+  }, [message, when])
+
+  const confirmExit = useCallback(() => {
+    const confirm = window.confirm(message)
+    return confirm
   }, [message])
-  return useBlocker(blocker, when)
+  useConfirmExit(confirmExit, when)
 }
