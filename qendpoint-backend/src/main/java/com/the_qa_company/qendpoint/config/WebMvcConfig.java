adee1ffe233b0a97f87e1089910afb900b641174
qaate47 <antoine.willerval@the-qa-company.com>
2025-01-09 13:37:04 +0100

    [skip ci] tests

--- qendpoint-backend/src/main/java/com/the_qa_company/qendpoint/config/WebMvcConfig.java
+++ qendpoint-backend/src/main/java/com/the_qa_company/qendpoint/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.the_qa_company.qendpoint.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +20,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //    registry
 //        .addViewController("/{x:^(?!api$).*$}/{y:[\\w\\-]+}")
 //        .setViewName("forward:/index.html");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 }
